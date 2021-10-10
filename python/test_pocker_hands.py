129c2f4e3a4dcc41bcfab5f40da26f1035f9d24e
Mason Lin <mason.lin1@hp.com>
2021-10-10 11:47:38 +0800

    refactor: sort test, public and internal

--- python/test_pocker_hands.py
+++ python/test_pocker_hands.py
@@ -17,9 +17,22 @@ def test_compare_highcard_10():
         player1="Black",
         cards1="2H 3D 5S 9C 10D",
         player2="White",
-        cards2="2C 3H 4S 8C AH",
+        cards2="2C 3H 4S 8C 9H",
     )
-    assert game.result() == "White wins. - with high card: A"
+    assert game.result() == "Black wins. - with high card: 10"
+
+
+def test_compare_fullhouse():
+    game = PokerGame(
+        player1="Black",
+        cards1="2S 8S AS QS 3S",
+        player2="White",
+        cards2="2H 4S 4C 2D 4H",
+    )
+    assert game.result() == "Black wins. - with full house: 4 over 2"
+
+
+################# test internal functions #########################
 
 
 def test_hand_card_parser():
@@ -49,14 +62,3 @@ def test_compare_number_size():
 
     output = game.compare_number_size("10", "A")
     assert output == -1
-
-
-# @pytest.mark.skip()
-def test_compare_fullhouse():
-    game = PokerGame(
-        player1="Black",
-        cards1="2S 8S AS QS 3S",
-        player2="White",
-        cards2="2H 4S 4C 2D 4H",
-    )
-    assert game.result() == "Black wins. - with full house: 4 over 2"
